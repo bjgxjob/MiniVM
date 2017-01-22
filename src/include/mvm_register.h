d4a3d0ad13b6bee3b49effa35533a5ccac64394e
yangrq <visualstudio2014@live.com>
2017-01-22 23:37:47 +0800

    add register

--- src/include/mvm_register.h
+++ src/include/mvm_register.h
@@ -5,4 +5,28 @@
 #include <stdint.h>
 #include <vector>
 
+#include "mvm_memory.h"
+
+namespace mvm
+{
+	struct mvm_register_block
+	{
+		mvm_mem_block data;
+		bool is_eax;
+		bool is_ebx;
+		bool is_ecx;
+		bool is_edx;
+		bool is_esp;
+		bool is_ebp;
+		bool is_esi;
+		bool is_edi;
+		bool is_eflags;
+		bool is_tsc;
+		bool is_eip;
+		bool is_seg;
+	};
+
+}
+
+
 #endif
